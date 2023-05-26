595d8bd8fd263fcad0d244e5fc2cb5c9a00a7530
EvanPiro <dev.evanpiro@gmail.com>
2023-05-25 22:37:43 -0400

    fix refd

--- test/SimpleStable.t.sol
+++ test/SimpleStable.t.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.8.16;
 
 import "forge-std/Test.sol";
-import "../src/SimpleStable.sol";
 
-contract SignedDataTest is Test {
+contract SimpleStable is Test {
     function setUp() public {
 
     }
